7902e894d777f26876f3c2bae502ace88fbf5e26
Vicky <vickyelfathea@gmail.com>
2022-06-24 23:21:44 +0700

    make better

--- src/modules/v1/vehicles/vehicle_route.go
+++ src/modules/v1/vehicles/vehicle_route.go
@@ -4,7 +4,8 @@ import (
 	// "net/http"
 	"github.com/gorilla/mux"
 	"gorm.io/gorm"
-	"carRent/src/middleware")
+	// "carRent/src/middleware"
+)
 
 func New(rt *mux.Router, db *gorm.DB) {
 	route := rt.PathPrefix("/vehicles").Subrouter()
@@ -15,7 +16,7 @@ func New(rt *mux.Router, db *gorm.DB) {
 
 	route.HandleFunc("/", ctrl.GetAll).Methods("GET")
 	route.HandleFunc("/", ctrl.AddData).Methods("POST")
-	route.HandleFunc("/update/{id}", middleware.Do(ctrl.UpdateData, middleware.CheckAuth)).Methods("PUT")
+	route.HandleFunc("/update/{id}", ctrl.UpdateData).Methods("PUT")
 	route.HandleFunc("/delete/{id}", ctrl.DeleteData).Methods("DELETE")
 	route.HandleFunc("/{id}", ctrl.GetById).Methods("GET")
 }
